506404d6cc556d4d5109f888c2674be6984f0cf5
David Shustin <david.shustin@gmail.com>
2017-08-10 21:34:23 -0400

    Add TPS4

---
+++ tps/TPS4.java
@@ -0,0 +1,18 @@
+package tps;
+import java.util.Random;
+public class TPS4 {
+  public static void main(String[] args) {
+    Random rand = new Random();
+    int[] nums = new int[10];
+    for (int i = 0; i < nums.length; i++) {
+      nums[i] = rand.nextInt();
+    }
+    int[] doubleSize = new int[nums.length * 2];
+    System.arraycopy(nums, 0, doubleSize, 0, nums.length);
+    nums = doubleSize;
+    System.out.println(nums.length);
+    for (int n : nums) {
+      System.out.println(n);
+    }
+  }
+}
