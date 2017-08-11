24e8dc2a5f252e8002b200534eb61904ddd2697f
David Shustin <david.shustin@gmail.com>
2017-08-11 17:24:06 -0400

    Fix Sort

--- projects/Sort.java
+++ projects/Sort.java
@@ -27,7 +27,7 @@ public class Sort {
     for (int i = 0; i < (arr.length - 1); i++) {
       for (int j = (i +1); j < arr.length; j++) {
         if (arr[i].compareTo(arr[j]) > 0) {
-          int temp = arr[i];
+          Comparable temp = arr[i];
           arr[i] = arr[j];
           arr[j] = temp;
         }
