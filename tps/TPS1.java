26e022c103c970746b8e3fd436182fe63cd8cfa6
David Shustin <david.shustin@gmail.com>
2017-08-10 21:04:30 -0400

    Fix TPS1

--- tps/TPS1.java
+++ tps/TPS1.java
@@ -1,14 +1,20 @@
 package tps;
 public class TPS1 {
 	public static void main(String[] args) {
-		String[] other = new String[args.length * 2];
-		for (int i = 0; i < args.length; i++) {
-			other[i] = args[i];
+		int[] nums = new int[10];
+		for (int i = 0; i < nums.length; i++) {
+			nums[i] = i;
 		}
-		args = other;
-		System.out.println(args.length);
-		for (int i = 0; i < args.length; i++) {
-			System.out.println(args[i]);
+		for (int i = 0; i < nums.length; i++) {
+			System.out.println(nums[i]);
+		}
+		String[] strs = new String[6];
+		for (String s : strs) {
+			System.out.println(s);
+		}
+		strs = new String[] {"meh", "cool", "name", "fox", "dog", "name"};
+		for (String s : strs) {
+			System.out.println(s);
 		}
 	}
 }
