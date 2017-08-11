c88b8abb3552f56849d2c59c2e801e1d2850a28a
David Shustin <david.shustin@gmail.com>
2017-08-10 23:29:42 -0400

    Fix TPS9

--- tps/TPS9.java
+++ tps/TPS9.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class TPS9 {
   public static void main(String[] args) {
     int[] arr1 = new int[] {3, 4, 4, 6264, 42, -23, 45, 1, 55, 545, -3241, -31};
-    int[] arr2 = new int[] {134, 341, 3, -31, 3, 451, -33, 34};
+    String[] arr2 = new String[] {"Shustin", "David", "Farrell", "Keenan", "Miron"};
     System.out.println("Before sort()");
     System.out.println(Arrays.toString(arr1));
     System.out.println(Arrays.toString(arr2));
@@ -24,7 +24,15 @@ public class TPS9 {
       }
     }
   }
-  public static void sort(int[] v, boolean overload) {
-    Arrays.sort(v);
+  public static void sort(String[] v) {
+    for (int c1 = 0; c1 < (v.length - 1); c1++) {
+      for (int c2 = (c1 +1); c2 < v.length; c2++) {
+        if (v[c1].compareTo(v[c2]) > 0) {
+          String temp = v[c1];
+          v[c1] = v[c2];
+          v[c2] = temp;
+        }
+      }
+    }
   }
 }
