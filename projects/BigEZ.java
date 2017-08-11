8e20ab2755baa5f95c013343207c97f5b844e57d
David Shustin <david.shustin@gmail.com>
2017-08-11 18:37:29 -0400

    Complete BigEZ

---
+++ projects/BigEZ.java
@@ -0,0 +1,23 @@
+package projects;
+public class BigEZ {
+  public static void main(String[] args) {
+    int N = 1000;
+    if (args.length > 0)
+      N = Integer.parseInt(args[0]);
+    int[] iterations = new int[N];
+    for (int i = 0; i < N; i++) {
+      int currIterationCount = 0;
+      double sum = 0;
+      while (sum <= 1) {
+        sum += Math.random();
+        currIterationCount++;
+      }
+      iterations[i] = currIterationCount;
+    }
+    int iterationsSum = 0;
+    for (int i : iterations) {
+      iterationsSum += i;
+    }
+    System.out.println("Average iterations: " + ((double) iterationsSum / N));
+  }
+}
