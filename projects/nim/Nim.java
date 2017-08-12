bd704b18c28955077c2920c55ed6270560b627c6
David Shustin <david.shustin@gmail.com>
2017-08-12 16:55:46 -0400

    Complete Nim

--- projects/nim/Nim.java
+++ projects/nim/Nim.java
@@ -3,7 +3,7 @@ import java.util.Stack;
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
-public class Nim extends Applet {//implements MouseEvent {
+public class Nim extends Applet implements MouseListener {
   Player bluePlayer, redPlayer, currentPlayer;
   Stack<Stone>[] stacks;
   final int STACK_LENGTH = 3;
@@ -11,11 +11,13 @@ public class Nim extends Applet {//implements MouseEvent {
   final Color[] STACK_COLORS = new Color[] {Color.red, Color.green, Color.blue};
   final int[] STACK_LOCATIONS = new int[] {30, 190, 350};
   final int STACK_WIDTH = 100;
+  final Rectangle nextButton = new Rectangle(10, 10, 75, 50);
   boolean gameOver;
   Player winner;
   int lockedStack;
   @SuppressWarnings("unchecked")
   public void init() {
+    addMouseListener(this);
     gameOver = false;
     lockedStack = -1;
     setBackground(Color.white);
@@ -47,10 +49,12 @@ public class Nim extends Applet {//implements MouseEvent {
         g.fillRect(stone.getX(), stone.getY(), stone.getWidth(), stone.getHeight());
       }
     }
-    if (empty) {
-      gameOver = true;
-      winner = otherPlayer();
-    }
+    g.setColor(Color.magenta);
+    g.drawRect((int)nextButton.getX(), (int)nextButton.getY(), (int)nextButton.getWidth(), (int)nextButton.getHeight());
+    g.fillRect((int)nextButton.getX(), (int)nextButton.getY(), (int)nextButton.getWidth(), (int)nextButton.getHeight());
+    g.setColor(Color.white);
+    g.drawString("NEXT TURN", (int)nextButton.getX() + 5, (int)(nextButton.getY() + nextButton.getHeight()/2 + 2));
+    if (empty) endGame();
   }
   private Stone selectedStone(int x, int y) {
     for (Stack<Stone> stack : stacks) {
@@ -62,7 +66,7 @@ public class Nim extends Applet {//implements MouseEvent {
   }
   private Stack<Stone> selectedStack(int x, int y) {
     for (Stack<Stone> stack : stacks) {
-      if (stack.peek().getRect().contains(x,y)) return stack;
+      if (!stack.empty() && stack.peek().getRect().contains(x,y)) return stack;
     }
     return null;
   }
@@ -72,10 +76,36 @@ public class Nim extends Applet {//implements MouseEvent {
   private void nextTurn() {
     lockedStack = -1;
     currentPlayer = otherPlayer();
+    repaint();
   }
   private boolean removeFromStack(int n) {
     if (lockedStack < 0) lockedStack = n;
     if (lockedStack == n) stacks[n].pop();
     return lockedStack == n;
   }
+  private void endGame() {
+    gameOver = true;
+    winner = otherPlayer();
+    repaint();
+  }
+  public void mouseClicked(MouseEvent e)
+  {
+    Stack<Stone> s = selectedStack(e.getX(), e.getY());
+    if (s != null) {
+      int i = 0;
+      for (i = 0; i < stacks.length; i++) {
+        if (stacks[i] == s) break;
+      }
+      removeFromStack(i);
+      repaint();
+    } else if (nextButton.contains(e.getX(), e.getY())) {
+      nextTurn();
+      repaint();
+    }
+  }
+  //Necessary Mouse events:
+  public void mousePressed(MouseEvent e) {}
+  public void mouseReleased(MouseEvent e) {}
+  public void mouseEntered(MouseEvent e) {}
+  public void mouseExited(MouseEvent e) {}
 }
